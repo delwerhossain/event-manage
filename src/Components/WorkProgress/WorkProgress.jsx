d5ae4f781127162461086f838d904b6a2d6c9046
delwerhossain <delwerhossain006@gmail.com>
2023-10-09 09:37:47 +0100

    WorkProgress chart main page create

--- src/Components/WorkProgress/WorkProgress.jsx
+++ src/Components/WorkProgress/WorkProgress.jsx
@@ -1,11 +1,18 @@
+import { BarChart } from "./BarChart";
 
 
 const WorkProgress = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const data = {
+    labels: ["January", "February", "March", "April", "May", "June"],
+    title: "Sales",
+    values: [100, 200, 300, 400, 500, 600],
+  };
+
+  return (
+    <div>
+      <BarChart data={data} />
+    </div>
+  );
 };
 
 export default WorkProgress;
