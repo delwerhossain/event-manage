b3297331eebad66ac1e41bab52430656e763e815
delwerhossain <delwerhossain006@gmail.com>
2023-10-09 16:02:56 +0100

    aos animation added

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,10 +1,17 @@
+import { useEffect } from "react";
 import Banner from "../Banner/Banner";
-
+import AOS from "aos";
+import "aos/dist/aos.css";
 import Services from "../Services/Services";
 
 const Home = () => {
+  //useEffect
+  useEffect(() => {
+    AOS.init();
+    AOS.refresh();
+  }, []);
   return (
-    <div>
+    <div data-aos="fade-up" data-aos-delay="150" data-aos-duration="2000">
       <Banner></Banner>
       <Services></Services>
     </div>
